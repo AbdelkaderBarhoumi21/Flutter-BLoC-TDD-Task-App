7086e5af4ead67ac6a23de66cbc7cb2d3d505838
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-01-01 20:24:51 +0100

    features:Add Task model test

---
+++ test/features/task/data/datasources/task_local_data_source_test.dart
@@ -0,0 +1,8 @@
+
+import 'package:mockito/annotations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+@GenerateMocks([SharedPreferences])
+void main(){
+
+}
--- test/features/task/data/models/task_model_test.dart
+++ test/features/task/data/models/task_model_test.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter_task_app/features/task/data/models/task_model.dart';
 import 'package:flutter_task_app/features/task/domain/entities/task_entity.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../fixtures/fixtures_reader.dart';
 
 void main() {
   final tTaskModel = TaskModel(
@@ -17,6 +21,33 @@ void main() {
   });
 
   group('FromJson', () {
-    test('Should return a valid model when json is valid', () {});
+    test('Should return a valid model when json is valid', () {
+      final Map<String, dynamic> jsonMap =
+          json.decode(fixture('task.json')) as Map<String, dynamic>;
+      // Act
+      final result = TaskModel.fromJson(jsonMap);
+
+      // Assert
+      expect(result, tTaskModel);
+    });
+  });
+
+  group('toJson', () {
+    test('Should return a JSON map containing proper data', () {
+      // Act
+      final result = tTaskModel.toJson();
+      final expectedMap = {
+        'id': '1',
+        'title': 'Test Task',
+        'description': 'Test Description',
+        'priority': 'high',
+        'status': 'pending',
+        'createdAt': '2024-01-01T00:00:00.000Z',
+        'completedAt': null,
+      };
+
+      // Assert
+      expect(result, expectedMap);
+    });
   });
 }
